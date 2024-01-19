1c9627099ea491922808b9595d001648a4325f24
LeighAllenDev <>
2024-01-19 23:34:44 +0000

    got front end set up

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // sidenav initialiization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});
