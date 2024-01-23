3a654fc2518dd9fdb41f7c3b2077e86cc49fc37d
LeighAllenDev <>
2024-01-23 22:37:26 +0000

    feat: add ability to add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialiization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm, yy",
+        i18n: { done: "Select" }
+    });
+
+    // selects
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
